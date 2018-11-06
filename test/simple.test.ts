768b9f1c11d58f9fbdd32d187a86101e9c93af41
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-11-05 22:21:49 -0500

    chore: update tests

--- test/simple.test.ts
+++ test/simple.test.ts
@@ -1,57 +1,57 @@
-import test from 'ava';
-import { stripIndent } from 'common-tags';
-import * as execa from 'execa';
-import { readFileSync } from 'fs';
-import { safeDump, safeLoad } from 'js-yaml';
-import { join } from 'path';
-import * as tempWrite from 'temp-write';
+import test from 'ava'
+import { stripIndent } from 'common-tags'
+import * as execa from 'execa'
+import { readFileSync } from 'fs'
+import { safeDump, safeLoad } from 'js-yaml'
+import { join } from 'path'
+import * as tempWrite from 'temp-write'
 
 const orbFilename = join(__dirname, '..', 'orb.yml')
 const orb = safeLoad(readFileSync(orbFilename, 'utf-8'))
 
-const validate = (filename: string):Promise<void> => {
+const validate = (filename: string): Promise<void> => {
   const cmd = `circleci config validate ${filename}`
-  return execa.shell(cmd, {stdio: 'inherit'})
+  return execa.shell(cmd, { stdio: 'inherit' })
 }
 
-const inlineOrb = (config:string):string => {
+const inlineOrb = (workflows: string): string => {
+  const intro = stripIndent`
+    version: 2.1
+    orbs:
+      cypress: cypress-io/cypress@1.0.0
+  `
+  const config = intro + '\n' + workflows
   const yaml = safeLoad(config)
   yaml.orbs.cypress = orb
   const inlined = safeDump(yaml)
   return inlined
 }
 
+const processWorkflows = (workflows: string): Promise<any> => {
+  const inlined = inlineOrb(workflows)
+  const filename = tempWrite.sync(inlined, 'config.yml')
+  return validate(filename)
+}
+
 test('simple', t => {
-  const config = stripIndent`
-    # simple example workflow
-    version: 2.1
-    orbs:
-      cypress: cypress-io/cypress@1.0.0
+  const workflows = stripIndent`
     workflows:
       build:
         jobs:
           - cypress/run
   `
-  t.is(typeof config, 'string')
-  const inlined = inlineOrb(config)
-  const filename = tempWrite.sync(inlined, 'config.yml')
-  return validate(filename)
+  t.is(typeof workflows, 'string')
+  return processWorkflows(workflows)
 })
 
 test('simple with record', t => {
-  const config = stripIndent`
-    # record test results on dashboard
-    version: 2.1
-    orbs:
-      cypress: cypress-io/cypress@1.0.0
+  t.plan(0)
+  const workflows = stripIndent`
     workflows:
       build:
         jobs:
           - cypress/run:
               record: true
   `
-  t.is(typeof config, 'string')
-  const inlined = inlineOrb(config)
-  const filename = tempWrite.sync(inlined, 'config.yml')
-  return validate(filename)
+  return processWorkflows(workflows)
 })
