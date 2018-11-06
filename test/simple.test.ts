ce9eac60f2f23a4cfedaee0a32dfed50daad1c84
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-11-05 22:25:31 -0500

    Test orb 18 (#19)
    * chore: working on unit testing orbs for #18
    
    * hmm, validate and process allows invalid commands
    
    * chore: fix tests
    
    * chore: update tests
    
    * parallel job test

---
+++ test/simple.test.ts
@@ -0,0 +1,75 @@
+import test from 'ava'
+import { stripIndent } from 'common-tags'
+import * as execa from 'execa'
+import { readFileSync } from 'fs'
+import { safeDump, safeLoad } from 'js-yaml'
+import { join } from 'path'
+import * as tempWrite from 'temp-write'
+
+const orbFilename = join(__dirname, '..', 'orb.yml')
+const orb = safeLoad(readFileSync(orbFilename, 'utf-8'))
+
+const validate = (filename: string): Promise<void> => {
+  const cmd = `circleci config validate ${filename}`
+  return execa.shell(cmd, { stdio: 'inherit' })
+}
+
+const inlineOrb = (workflows: string): string => {
+  const intro = stripIndent`
+    version: 2.1
+    orbs:
+      cypress: cypress-io/cypress@1.0.0
+  `
+  const config = intro + '\n' + workflows
+  const yaml = safeLoad(config)
+  yaml.orbs.cypress = orb
+  const inlined = safeDump(yaml)
+  return inlined
+}
+
+const processWorkflows = (workflows: string): Promise<any> => {
+  const inlined = inlineOrb(workflows)
+  const filename = tempWrite.sync(inlined, 'config.yml')
+  return validate(filename)
+}
+
+test('simple', t => {
+  const workflows = stripIndent`
+    workflows:
+      build:
+        jobs:
+          - cypress/run
+  `
+  t.is(typeof workflows, 'string')
+  return processWorkflows(workflows)
+})
+
+test('simple with record', t => {
+  t.plan(0)
+  const workflows = stripIndent`
+    workflows:
+      build:
+        jobs:
+          - cypress/run:
+              record: true
+  `
+  return processWorkflows(workflows)
+})
+
+test('parallel 2 machines', t => {
+  t.plan(0)
+  const workflows = stripIndent`
+    workflows:
+      build:
+        jobs:
+          - cypress/install
+          - cypress/run:
+              requires:
+                - cypress/install
+              record: true
+              parallel: true
+              parallelism: 2
+              group: "2 machines"
+  `
+  return processWorkflows(workflows)
+})
