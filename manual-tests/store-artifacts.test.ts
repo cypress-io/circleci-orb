b96f3f68c6d26593c71baa0378889c52ef9388b7
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-04-20 13:37:41 -0400

    fix: store artifacts in working directory (#268)
    * fix: use working directory in store artifacts steps
    
    * fix: skip empty build command

---
+++ manual-tests/store-artifacts.test.ts
@@ -0,0 +1,27 @@
+import test from 'ava'
+import { stripIndent } from 'common-tags'
+import { effectiveConfig } from '../scripts/utils'
+
+// https://github.com/cypress-io/circleci-orb/issues/265
+test('store video and screenshots in working directory', async t => {
+  const workflows = stripIndent`
+    workflows:
+      build:
+        jobs:
+          - cypress/run:
+              working_directory: path/to/subfolder
+              store_artifacts: true
+  `
+  t.is(typeof workflows, 'string')
+  const result = await effectiveConfig(workflows)
+
+  // we are only interested in some lines, not entire config
+  const storeArtifactsAt = result.search(/\n\s+- store_artifacts:/)
+  t.true(storeArtifactsAt > 20, 'cannot find store artifacts')
+  const workflowsAt = result.indexOf('workflows:', storeArtifactsAt)
+  t.true(workflowsAt > 0, 'cannot find workflows')
+
+  const fragment = result.slice(storeArtifactsAt, workflowsAt)
+  // fragment should include "path/to/subfolder" prefix
+  t.snapshot(fragment, 'store artifacts fragment')
+})
