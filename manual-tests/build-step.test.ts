d477446c24aa847d63fbbc0bb72d2ba3c30e5cde
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-04-20 13:07:06 -0400

    fix: do no skip build step when attaching workspace in run job (#267)
    * fix: include the build step after attaching workspace
    
    * update issue number

---
+++ manual-tests/build-step.test.ts
@@ -0,0 +1,21 @@
+import test from 'ava'
+import { stripIndent } from 'common-tags'
+import { effectiveConfig } from '../scripts/utils'
+
+// https://github.com/cypress-io/circleci-orb/issues/266
+test('build step works after attaching workspace', async t => {
+  // we want to attach the workspace, probably after a shared install job
+  // and build the application - and the effective config
+  // should have the build step
+  const workflows = stripIndent`
+    workflows:
+      build:
+        jobs:
+          - cypress/run:
+              attach-workspace: true
+              build: npm run build
+  `
+  t.is(typeof workflows, 'string')
+  const result = await effectiveConfig(workflows)
+  t.snapshot(result, 'must include build step')
+})
