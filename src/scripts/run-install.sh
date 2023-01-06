e2c25d8d306c39d50634d22a0b1b21a8e994143a
Jordan <jordan@jpdesigning.com>
2023-01-06 15:57:47 -0500

    fix: add missing space in bash expression

--- src/scripts/run-install.sh
+++ src/scripts/run-install.sh
@@ -12,7 +12,7 @@ if [ -f "$YARN" ]; then
     echo "yarn install --frozen-lockfile"
     yarn install --frozen-lockfile
 
-if [ -f "$PACKAGELOCK"]; then
+if [ -f "$PACKAGELOCK" ]; then
     echo "The Cypress orb uses 'npm ci' to install 'node_modules', which requires a 'package-lock.json'."
     echo "A 'package-lock.json' file was not found. Please run 'npm install' in your project,"
     echo "and commit 'package-lock.json' to your repo."
