17680c774773a70f94f350fa8067ca61e43a72b8
Jordan <jordan@jpdesigning.com>
2023-01-06 15:59:14 -0500

    fix: change else if syntax

--- src/scripts/run-install.sh
+++ src/scripts/run-install.sh
@@ -7,12 +7,12 @@ if [ "${INSTALL_COMMAND}" ]; then
     echo "${INSTALL_COMMAND}"
     ${INSTALL_COMMAND}
 
-if [ -f "$YARN" ]; then
+elif [ -f "$YARN" ]; then
     echo "Installing using Yarn"
     echo "yarn install --frozen-lockfile"
     yarn install --frozen-lockfile
 
-if [ -f "$PACKAGELOCK" ]; then
+elif [ -f "$PACKAGELOCK" ]; then
     echo "The Cypress orb uses 'npm ci' to install 'node_modules', which requires a 'package-lock.json'."
     echo "A 'package-lock.json' file was not found. Please run 'npm install' in your project,"
     echo "and commit 'package-lock.json' to your repo."
