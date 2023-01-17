cf698417087a5f8cce924fae9568911913ac7189
Jordan <jordan@jpdesigning.com>
2023-01-17 14:03:31 -0500

    ref(examples): create 3 example apps

--- examples/custom-install/cypress.config.js
+++ examples/custom-install/cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   fixturesFolder: false,
   e2e: {
-    setupNodeEvents(on, config) {},
     supportFile: false,
+    video: false
   },
 })
