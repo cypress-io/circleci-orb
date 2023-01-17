aaf1aca16e65609090776a199023a96583e97147
Jordan <jordan@jpdesigning.com>
2023-01-17 14:22:26 -0500

    fix(scripts): use working-directory directly in if else logic

--- src/scripts/run-install.sh
+++ src/scripts/run-install.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-YARN=${WORKING_DIRECTORY}/yarn.lock
-PACKAGELOCK=${WORKING_DIRECTORY}/package-lock.json
+# YARN=${WORKING_DIRECTORY}/yarn.lock
+# PACKAGELOCK=${WORKING_DIRECTORY}/package-lock.json
+
+echo "${WORKING_DIRECTORY}"
 
 if [ "${INSTALL_COMMAND}" ]; then
     echo "Installing using custom command"
     echo "${INSTALL_COMMAND}"
     ${INSTALL_COMMAND}
 
-elif [ -f "${YARN}" ]; then
+elif [ -f "${WORKING_DIRECTORY}/yarn.lock" ]; then
     echo "Installing using Yarn"
     echo "yarn install --frozen-lockfile"
     yarn install --frozen-lockfile
 
-elif [ -f "${PACKAGELOCK}" ]; then
+elif [ -f "${WORKING_DIRECTORY}/package-lock.json" ]; then
     echo "The Cypress orb uses 'npm ci' to install 'node_modules', which requires a 'package-lock.json'."
     echo "A 'package-lock.json' file was not found. Please run 'npm install' in your project,"
     echo "and commit 'package-lock.json' to your repo."
