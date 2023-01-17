9c64c398eb46c584380ed2dc43c28f9d21d52a77
Jordan <jordan@jpdesigning.com>
2023-01-17 14:33:00 -0500

    fix(scripts): revert install logic

--- src/scripts/run-install.sh
+++ src/scripts/run-install.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
-# YARN=${WORKING_DIRECTORY}/yarn.lock
-# PACKAGELOCK=${WORKING_DIRECTORY}/package-lock.json
-
-echo "${WORKING_DIRECTORY}"
+YARN=${WORKING_DIRECTORY}/yarn.lock
+PACKAGELOCK=${WORKING_DIRECTORY}/package-lock.json
 
 if [ "${INSTALL_COMMAND}" ]; then
     echo "Installing using custom command"
     echo "${INSTALL_COMMAND}"
     ${INSTALL_COMMAND}
 
-elif [ -f "${WORKING_DIRECTORY}/yarn.lock" ]; then
+elif [ -f "$YARN" ]; then
     echo "Installing using Yarn"
     echo "yarn install --frozen-lockfile"
     yarn install --frozen-lockfile
 
-elif [ -f "${WORKING_DIRECTORY}/package-lock.json" ]; then
+elif [ -f "$PACKAGELOCK" ]; then
     echo "The Cypress orb uses 'npm ci' to install 'node_modules', which requires a 'package-lock.json'."
     echo "A 'package-lock.json' file was not found. Please run 'npm install' in your project,"
     echo "and commit 'package-lock.json' to your repo."
