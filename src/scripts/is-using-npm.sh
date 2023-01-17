f2c46a14c992860ce6943cd50e077f128049ab20
Paul Jaffre <jaffrepaul@gmail.com>
2023-01-17 13:45:35 -0500

    wip: adds test project (#395)
    * add npm-test-project
    
    * add yarn-test-project
    
    * add custom-install-project
    
    * basic machine flow
    
    * basic test example flow
    
    * chore: move cypress run into workflow
    
    * test
    
    * log dir after checkout
    
    * call workflow directly
    
    * move ls after checkout
    
    * checkout, run, restore cache
    
    * add logging
    
    * test condition
    
    * fix: save_cache step to be conditional
    
    * fix: fix empty space lint error
    
    Co-authored-by: Jordan <jordan@jpdesigning.com>

--- src/scripts/is-using-npm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-PACKAGELOCK=/${WORKING_DIRECTORY}/package-lock.json
-if [ -f "$PACKAGELOCK" ]; then
-    true
-
-else
-    false
-
-fi
--- src/scripts/is-using-yarn.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-YARN=/${WORKING_DIRECTORY}/yarn.lock
-if [ -f "$YARN" ]; then
-    true
-
-else
-    false
-
-fi
