ed9de1ece78e3fb94b942987148ab499ffd3b95e
Paul Jaffre <jaffrepaul@gmail.com>
2023-01-17 11:06:55 -0500

    add logging

--- src/scripts/is-using-npm.sh
+++ src/scripts/is-using-npm.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 PACKAGELOCK=/${WORKING_DIRECTORY}/package-lock.json
 if [ -f "$PACKAGELOCK" ]; then
+    echo "$PACKAGELOCK"
+    echo true
     true
 
 else
+    echo "$PACKAGELOCK"
+    echo false
     false
 
 fi
--- src/scripts/is-using-yarn.sh
+++ src/scripts/is-using-yarn.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 YARN=/${WORKING_DIRECTORY}/yarn.lock
 if [ -f "$YARN" ]; then
+    echo "$YARN"
+    echo true
     true
-
 else
+    echo "$YARN"
+    echo false
     false
 
 fi
