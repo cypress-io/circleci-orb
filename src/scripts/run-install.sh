f36aa12ed84a903e1c360ae31b2286cd49de0016
Jordan <jordan@jpdesigning.com>
2023-01-17 15:13:56 -0500

    fix(scripts): add -e flags to if logic

--- src/scripts/run-install.sh
+++ src/scripts/run-install.sh
@@ -7,13 +7,13 @@ if [ "${INSTALL_COMMAND}" ]; then
     echo "${INSTALL_COMMAND}"
     ${INSTALL_COMMAND}
 
-elif [ -f "$YARN" ]; then
+elif [ -e "$YARN" ]; then
     echo "Installing using Yarn"
     echo "yarn install --frozen-lockfile"
     echo "Install in ${WORKING_DIRECTORY}"
     yarn install --frozen-lockfile
 
-elif [ -f "$PACKAGELOCK" ]; then
+elif [ ! -e "$PACKAGELOCK" ]; then
     echo "The Cypress orb uses 'npm ci' to install 'node_modules', which requires a 'package-lock.json'."
     echo "A 'package-lock.json' file was not found. Please run 'npm install' in your project,"
     echo "and commit 'package-lock.json' to your repo."
