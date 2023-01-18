5dbb1f03391f9faa7c33ae7a3bd76636cc4e07b3
Jordan <jordan@jpdesigning.com>
2023-01-18 16:14:41 -0500

    chore(scripts): update scripts to cd into DIR before executing install

--- src/scripts/run-install.sh
+++ src/scripts/run-install.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
-YARN=${WORKING_DIRECTORY}/yarn.lock
-PACKAGELOCK=${WORKING_DIRECTORY}/package-lock.json
+
+DIR=${WORKING_DIRECTORY}
+
+echo "${DIR}"
 
 if [ "${INSTALL_COMMAND}" ]; then
     echo "Installing using custom command"
     echo "${INSTALL_COMMAND}"
+    cd ${DIR}
     ${INSTALL_COMMAND}
 
-elif [ -f "$YARN" ]; then
+elif [ -f "${DIR}/yarn.lock" ]; then
     echo "Installing using Yarn"
     echo "yarn install --frozen-lockfile"
+    cd ${DIR}
     yarn install --frozen-lockfile
 
-elif [ ! -f "$PACKAGELOCK" ]; then
+elif [ ! -f "${DIR}/package-lock.json" ]; then
     echo "The Cypress orb uses 'npm ci' to install 'node_modules', which requires a 'package-lock.json'."
     echo "A 'package-lock.json' file was not found. Please run 'npm install' in your project,"
     echo "and commit 'package-lock.json' to your repo."
@@ -21,6 +25,7 @@ elif [ ! -f "$PACKAGELOCK" ]; then
 else
     echo "Installing dependencies using NPM ci"
     echo "Install in ${WORKING_DIRECTORY}"
+    cd ${DIR}
     npm ci
 
 fi
